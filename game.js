fade18635333867cdc27e3218ee0bcf978b774fb
Divyansh Dhaundiyal <112102507+Divyansh1316@users.noreply.github.com>
2024-04-11 19:24:24 +0530

    Update game.js

--- game.js
+++ game.js
@@ -4,7 +4,7 @@ var buttonColours = ["red", "blue", "green", "yellow"];
 
 var started = false;
 var level = 0;
-$(document).on("keydown click", function () {
+$(document).keydown(function () {
   if (!started) {
     $("#level-title").text("Level " + level);
     nextSequence();
